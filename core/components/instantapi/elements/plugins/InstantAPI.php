a1eef6346b7016d0df2db93820e291ee20600e33
Aaron Ladage <aladage@gmail.com>
2012-08-08 14:34:37 -0500

    First working version. Does not properly process tags inside [[*content]] yet, but the guts of it work fine.

---
+++ core/components/instantapi/elements/plugins/InstantAPI.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *
+ * InstantAPI
+ *
+ * A MODX plugin that instantly turns any MODX resource into a JSON feed, just by adding ".json" to the end of the URL
+ *
+ * @ author Aaron Ladage
+ * @ version 1.0.0 - August 8, 2012
+ *
+**/
+
+/* Set parameters */
+$uri = $_SERVER["REQUEST_URI"]; // Get the URI
+$extension = pathinfo($uri, PATHINFO_EXTENSION); // Get the extension from the URI
+$cleanedUri = str_replace(array('.json'),'',$uri);
+$cleanedUri = preg_replace('/\?.*/', '', $cleanedUri);
+$error_page = $modx->getOption('error_page');
+
+if ($extension != 'ico') {
+    $page = $modx->getObject('modResource',array('uri' => $cleanedUri));
+    if ($page) {
+        $fields = $page->toArray();
+        $fields['url'] = $modx->getOption('site_url').$page->get('uri');
+        $json = $modx->toJSON($fields);
+        session_write_close();
+        die($json);
+    }
+    else {
+        $modx->sendForward($error_page);
+    }
+}
