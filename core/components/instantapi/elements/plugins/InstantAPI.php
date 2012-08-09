dd12840def9196c042bbbf8d278c04f73b98b61f
Aaron Ladage <aladage@gmail.com>
2012-08-08 20:35:24 -0500

    Removed 404 redirect when matching page is not found, to prevent screwing up other 404 processing

--- core/components/instantapi/elements/plugins/InstantAPI.php
+++ core/components/instantapi/elements/plugins/InstantAPI.php
@@ -17,7 +17,6 @@ $uri = $_SERVER["REQUEST_URI"]; // Get the URI
 $extension = pathinfo($uri, PATHINFO_EXTENSION); // Get the extension from the URI
 $cleanedUri = str_replace(array('.json'),'',$uri);
 $cleanedUri = preg_replace('/\?.*/', '', $cleanedUri);
-$error_page = $modx->getOption('error_page');
 
 if (($extension != 'ico') && ($extension == 'json')) {
     $page = $modx->getObject('modResource',array('uri' => $cleanedUri));
@@ -27,8 +26,5 @@ if (($extension != 'ico') && ($extension == 'json')) {
         $json = $modx->toJSON($fields);
         session_write_close();
         die($json);
-    }
-    else {
-        $modx->sendForward($error_page);
     }
 }
