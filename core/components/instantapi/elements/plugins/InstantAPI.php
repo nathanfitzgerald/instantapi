b7e39368b629cfd94bc1227cf6a000e96e6259dd
Aaron Ladage <aladage@ku.edu>
2012-11-27 09:57:10 -0600

    Fairly substantial rewrite. Still does not work with TVs, but ready for a beta release

--- core/components/instantapi/elements/plugins/InstantAPI.php
+++ core/components/instantapi/elements/plugins/InstantAPI.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  *
  * InstantAPI
@@ -7,24 +6,25 @@
  * A MODX plugin that instantly turns any MODX resource into a JSON feed, just by adding ".json" to the end of the URL
  *
  * @ author Aaron Ladage
- * @ version 1.0.0 - August 8, 2012
+ * @ version 1.0.1 - November 27, 2012
  *
- * Save as a plugin, and check the OnPageNotFound system event.
 **/
 
-/* Set parameters */
 $uri = $_SERVER["REQUEST_URI"]; // Get the URI
-$extension = pathinfo($uri, PATHINFO_EXTENSION); // Get the extension from the URI
-$cleanedUri = str_replace(array('.json'),'',$uri);
-$cleanedUri = preg_replace('/\?.*/', '', $cleanedUri);
+$uri_array = parse_url($uri);
+$error_page = $modx->getOption('error_page'); // Get the error page
 
-if (($extension != 'ico') && ($extension == 'json')) {
+if (isset($uri_array['path']) && preg_match('/\.json$/i',$uri_array['path'])) {
+    $cleanedUri = str_replace(array('.json','/'),'',$uri); 
+    $cleanedUri = preg_replace('/\?.*/', '', $cleanedUri);
     $page = $modx->getObject('modResource',array('uri' => $cleanedUri));
     if ($page) {
-        $fields = $page->toArray();
-        $fields['url'] = $modx->getOption('site_url').$page->get('uri');
+        $fields = $page->toArray();        
         $json = $modx->toJSON($fields);
         session_write_close();
         die($json);
+    }
+    else {
+        $modx->sendForward($error_page);
     }
 }
